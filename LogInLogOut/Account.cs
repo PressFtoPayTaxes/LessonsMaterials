f7eb587a955f1be1434ead78ac6b483c3e524479
Игорь Валерьевич Скоропад <@corp.itstep>
2019-03-12 18:15:56 +0600

    Init

---
+++ LogInLogOut/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogInLogOut
+{
+    [Serializable]
+    public class Account
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Password { get; set; }
+    }
+}
