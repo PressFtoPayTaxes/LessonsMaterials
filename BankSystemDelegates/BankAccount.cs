114518df1fca94d37ab2ed9d466731415132a8c1
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-03-25 16:37:18 +0600

    Init

---
+++ BankSystemDelegates/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystemDelegates
+{
+    public class BankAccount
+    {
+        public string FullName { get; set; }
+        public int Sum { get; private set; } = 0;
+        public IReporter Reporter { get; set; }
+         
+        public void Add(int sum)
+        {
+            Sum += sum;
+            if (Reporter != null)
+                Reporter.SendReport($"На Ваш счёт перечислено {sum} тенге\nВаш текущий баланс: {Sum} тенге");
+        }
+
+        public void Withdraw(int sum)
+        {
+            if (sum <= Sum)
+            { 
+                Sum -= sum;
+                if (Reporter != null)
+                    Reporter.SendReport($"С Вашего счёта снято {sum} тенге\nВаш текущий баланс: {Sum} тенге");
+                return;
+            }
+            if (Reporter != null)
+                Reporter.SendReport("На Вашем счёте недостаточно средств для снятия данной суммы");
+        }
+    }
+}
---
+++ BankSystemDelegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystemDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount
+            {
+                FullName = "Сапарбек Иванович Дзюба",
+                Reporter = new ConsoleReporter()
+            };
+
+            account.Add(1000);
+            account.Withdraw(20000);
+            account.Withdraw(200);
+        }
+    }
+}
