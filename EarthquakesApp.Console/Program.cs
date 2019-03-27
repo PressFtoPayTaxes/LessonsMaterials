5419cf20cf9b6a712d7bb701c6e3ce6aef520733
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-03-27 18:27:56 +0600

    Init

---
+++ EarthquakesApp.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using EarthquakesApp.Models;
+using EarthquakesApp.Services;
+using EarthquakesApp.Services.Abstract;
+using Newtonsoft.Json;
+
+namespace EarthquakesApp.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new FileLogger();
+            IDownloader downloader = new Downloader(logger);
+            IRepository<FeatureCollection> repository = new XmlRepository<FeatureCollection>(logger);
+
+            var data = downloader.DownloadInfo("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=50");
+
+            if (!string.IsNullOrEmpty(data))
+            {
+                //(V)oO(V)
+                var deserializedData = JsonConvert.DeserializeObject<FeatureCollection>(data);
+                repository.Add(deserializedData);
+            }
+            else
+            {
+                System.Console.WriteLine("Произошла ошибка, обратитесь к системному администратору.");
+                System.Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ EarthquakesApp.Services.Abstract/ILogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EarthquakesApp.Services.Abstract
+{
+    public interface ILogger
+    {
+        void LogError(Exception exception);
+        void LogMessage(string text);
+    }
+}
