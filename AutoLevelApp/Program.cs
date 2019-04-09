627e8402fec9aba2eec018de1b33ee47bc657efa
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-04-09 16:28:11 +0600

    Init

---
+++ AutoLevelApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace AutoLevelApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataSet dataSet = new DataSet("books");
+            dataSet.ExtendedProperties.Add("OwnerCompany", "ITStep");
+            dataSet.ExtendedProperties.Add("CreationDate", DateTime.Now);
+
+            #region Books Table
+            DataTable booksTable = new DataTable("books");
+            booksTable.Columns.Add(new DataColumn
+            {
+                AllowDBNull = false,
+                AutoIncrement = true,
+                AutoIncrementSeed = 1,
+                AutoIncrementStep = 1,
+                ColumnName = "id",
+                DataType = typeof(int),
+                Unique = true
+            });
+            booksTable.PrimaryKey = new DataColumn[] { booksTable.Columns["id"] };
+
+            booksTable.Columns.Add("name", typeof(string));
+            booksTable.Columns.Add("price", typeof(int));
+            booksTable.Columns.Add("authorId", typeof(int));
+
+            dataSet.Tables.Add(booksTable);
+            #endregion
+
+            #region Authors Table
+            DataTable authorsTable = new DataTable("authors");
+            booksTable.Columns.Add(new DataColumn
+            {
+                AllowDBNull = false,
+                AutoIncrement = true,
+                AutoIncrementSeed = 1,
+                AutoIncrementStep = 1,
+                ColumnName = "id",
+                DataType = typeof(int),
+                Unique = true
+            });
+            authorsTable.PrimaryKey = new DataColumn[] { authorsTable.Columns["id"] };
+            authorsTable.Columns.Add("name", typeof(string));
+            dataSet.Tables.Add(authorsTable);
+            #endregion
+
+            authorsTable.Rows.Add(new object[] { "Пушкин А.С." });
+
+            DataRow row = booksTable.NewRow();
+            row.BeginEdit();
+            row.ItemArray = new object[] { "Сказки", 1000, 1 };
+            row.EndEdit();
+            row.SetAdded();
+            booksTable.Rows.Add(row);
+
+            DataRelation dataRelation = new DataRelation("authors_books_fk", "authors", "books", new string[] { "id" }, new string[] { "authorId" }, false);
+            dataSet.Relations.Add(dataRelation);
+        }
+    }
+}
